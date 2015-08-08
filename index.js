1b4937c27dbc671c05a9b8b3eaeeb4e7ae845be4
Toby Flemming <tobyf93@hotmail.com>
2015-08-08 11:00:19 +1000

    Receive and process valid JSON request.  Return JSON response.

--- index.js
+++ index.js
@@ -10,9 +10,30 @@ app.get('/', function (req, res) {
   res.send('This is a test!');
 });
 
+app.post('/', function(req, res) {
+  res.set('Content-Type', 'application/json');
+
+  resBody = processData(req.body);
+  res.send(JSON.stringify(resBody));
+});
+
 var server = app.listen(3000, function () {
   var host = server.address().address;
   var port = server.address().port;
 
   console.log('Example app listening at http://%s:%s', host, port);
 });
+
+var processData = function(data) {
+  return data.payload
+    .filter(function(item) {
+      return item.drm === true && item.episodeCount > 0;
+    })
+    .map(function(item) {
+      return {
+        image: item.image.showImage,
+        slug: item.slug,
+        title: item.title
+      };
+    });
+};
